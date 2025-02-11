6d8fa562e04c03fc8f28888924f8e4aa89e9f5f1
James A. Fellows Yates <jfy133@gmail.com>
2025-02-11 17:00:31 +0100

    Make better output

--- src/scripts/table-to-combinationclass.sh
+++ src/scripts/table-to-combinationclass.sh
@@ -1,21 +1,24 @@
 #! /usr/bin/env bash
 
-## Requires a tab-separated table with the following format:
-## Checklist	Extension 1	Extension 2	Existing Combination	Full name	Example
+## Requires a comma-separated table with the following format:
+## Checklist	ChecklistURI	Extension 1	CheckList2URI	Extension 2	Existing Combination	Full name	Example
 
 table=$1
 
-while read line; do
-    checklist=$(echo "$line" | cut -d, -f1)
-    checklist_uri=$(echo "$line" | cut -d, -f2)
-    extension=$(echo "$line" | cut -d, -f3)
-    extension_uri=$(echo "$line" | cut -d, -f4)
-    checklist_lower=$(echo $checklist | tr '[:upper:]' '[:lower:]')
-    extension_lower=$(echo $extension | tr '[:upper:]' '[:lower:]')
-    #example=$(echo "$line" | cut -d, -f6)
+## TODO: FIX BUG THAT INCLUDES FIRST LINE -> ALMOST FIXED, CHECK
 
-    echo "SLOT #########################"
-    echo "${checklist_lower}_${extension_lower}_ancient_data
+while read line; do
+
+  checklist=$(echo "$line" | cut -d, -f1)
+  checklist_uri=$(echo "$line" | cut -d, -f2)
+  extension=$(echo "$line" | cut -d, -f3)
+  extension_uri=$(echo "$line" | cut -d, -f4)
+  checklist_lower=$(echo $checklist | tr '[:upper:]' '[:lower:]')
+  extension_lower=$(echo $extension | tr '[:upper:]' '[:lower:]')
+  #example=$(echo "$line" | cut -d, -f6)
+
+  echo "SLOT #########################"
+  echo "${checklist_lower}_${extension_lower}_ancient_data:
   description: Data that comply with Ancient combined with ${checklist}${extension}
   title: ${checklist}${extension}Ancient Data
   domain: MixsCompliantData
@@ -24,10 +27,10 @@ while read line; do
   range: ${checklist}${extension}Ancient
   inlined: true
   inlined_as_list: true
-    "
+    " >>minas-combination-slots.txt
 
-    echo "CLASSES #########################"
-    echo "${checklist}${extension}Ancient
+  echo "CLASSES #########################"
+  echo "${checklist}${extension}Ancient:
   description: MIxS Data that comply with the ${checklist} checklist, and ${extension} and Ancient extensions.
   title: ${checklist}${extension} combined with Ancient
   in_subset:
@@ -35,6 +38,9 @@ while read line; do
   is_a: Ancient
   mixins:
       - ${checklist}${extension}
-  class_uri: MIXS:${checklist_uri}_${extension_uri}_9999903"
+  class_uri: MIXS:${checklist_uri}_${extension_uri}_9999903" >>minas-combination-classes.txt
 
-done <$table
+  echo "COMPLIANT_DATA_SLOTS #########################"
+  echo "- ${checklist_lower}_${extension_lower}_ancient_data" >>minas-combination-compliantdataslots.txt
+
+done <<<"$(tail +2 $table)"
